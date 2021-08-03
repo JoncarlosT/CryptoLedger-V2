4aa508611d7b141ea9b33b6709dab59825df2e56
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2021-08-02 22:00:16 -0400

    graphql user return cryptocoin working

--- server/schema/types/user_type.js
+++ server/schema/types/user_type.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLID } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLList } = graphql;
+const User = mongoose.model("user");
+const CryptoCoinType = require("./cryptocoin_type");
 
 const UserType = new GraphQLObjectType({
   name: "UserType",
@@ -8,6 +10,14 @@ const UserType = new GraphQLObjectType({
     _id: { type: GraphQLID },
     name: { type: GraphQLString },
     email: { type: GraphQLString },
+    cryptoWallet: {
+      type: new GraphQLList(CryptoCoinType),
+      resolve({ _id }) {
+        return User.findById(_id)
+          .populate("cryptoWallet")
+          .then((user) => user.cryptoWallet);
+      },
+    },
   }),
 });
 
