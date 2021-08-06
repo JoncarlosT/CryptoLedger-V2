48ebaa1f1532b56b439cbd70f15e022ab19a07a7
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2021-08-05 23:51:10 -0400

    graphql fetchcoin numofcoins added

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -45,9 +45,12 @@ const RootQueryType = new GraphQLObjectType({
 
     fetchCoins: {
       type: new GraphQLList(FetchCoinType),
-      async resolve() {
+      args: {
+        numOfCoins: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      async resolve(_, { numOfCoins }) {
         const response = await fetch(
-          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+          `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${numOfCoins}&page=1&sparkline=false`
         ).then((res) => res.json());
 
         return response;
