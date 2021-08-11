0716849a1c4629b46a075264fa798a623bae265e
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2021-08-11 19:57:22 -0400

    searchbar working

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import data from "../../data/coin_storage.json";
+import { createFilter } from "react-select";
 
 import { StyledNavbar, Logo, SearchBar } from "./styles";
 
 const Navbar = () => {
-  console.log(data);
+  const history = useHistory();
+  const [coinId, setCoinId] = useState("");
+
+  const handleSelect = () => {
+    history.push(`/coins/${coinId}`);
+  };
+
   return (
     <StyledNavbar>
       <Logo to="/">Home</Logo>
-      <SearchBar options={data} />
+      <SearchBar
+        filterOption={createFilter({ ignoreAccents: false })}
+        options={data}
+        getOptionLabel={(option) => option.name}
+        getOptionValue={(option) => option.id}
+        value={coinId}
+        onChange={(e) => setCoinId(e.id)}
+        onSubmit={handleSelect()}
+      />
     </StyledNavbar>
   );
 };
