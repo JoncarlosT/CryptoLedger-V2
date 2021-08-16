ac197e7d693174e7fa97f5b3ebed22d8f7e11bbf
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2021-08-15 20:47:37 -0400

    UserPage coin display working

--- client/src/pages/UserPage/UserPage.js
+++ client/src/pages/UserPage/UserPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
+import CryptoCoinChart from "../../components/CryptoCoinChart/CryptoCoinChart";
 import { FETCH_SINGLE_USER } from "../../graphql/queries";
 
 const UserPage = ({ userData }) => {
@@ -13,12 +14,28 @@ const UserPage = ({ userData }) => {
   if (error) return <h1>{error}</h1>;
 
   const { user } = data;
-  console.log(user);
+  console.log();
 
   return (
     <div>
       <h1>{user.name}</h1>
       <h1>{user.email}</h1>
+
+      {user.cryptoWallet.length === 0 ? (
+        <h1>Add coins</h1>
+      ) : (
+        user.cryptoWallet.map((coin, idx) => {
+          return (
+            <div key={idx}>
+              <h1>{coin.name}</h1>
+              <CryptoCoinChart coinId={coin.name} days={5} full={true} />
+              <p>Amount{coin.amount}</p>
+              <p>BuyPrice{coin.buyPrice}</p>
+              <p>CryptoImage{coin.cryptoImage}</p>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 };
