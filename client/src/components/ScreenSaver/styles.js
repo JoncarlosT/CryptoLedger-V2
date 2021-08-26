3eb9a42bc2dc1d4c495f796fee720ab28e83bac3
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2021-08-26 16:39:35 -0400

    ScreenSaver working, and added to cryptocoinindex

---
+++ client/src/components/ScreenSaver/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledScreenSaver = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 90vh;
+`;
+
+export const LogoImage = styled.img`
+  width: 400px;
+`;
+
+export const Header = styled.h1``;
