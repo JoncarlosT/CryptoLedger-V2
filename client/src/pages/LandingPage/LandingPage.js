98ad702e70be5dd3abfda845c85c00fd8e1a5dba
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2021-08-30 23:23:32 -0400

    landingpage development

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -2,12 +2,27 @@ import React from "react";
 
 import CryptoCoinIndex from "../../components/CryptoCoinIndex/CryptoCoinIndex";
 
-import { StyledLandingPage, CryptoCoinIndexWrapper } from "./styles";
+import {
+  StyledLandingPage,
+  CryptoCoinIndexWrapper,
+  LandingPageText,
+  LandingPageHeader,
+} from "./styles";
 
 export default function LandingPage() {
   return (
     <StyledLandingPage>
       <CryptoCoinIndexWrapper>
+        <LandingPageHeader>
+          <LandingPageText>Welcome to CryptoLedger</LandingPageText>
+          <LandingPageText>
+            We'll help you keep track of all crypto Cryptocurrencies
+          </LandingPageText>
+          <LandingPageText>
+            Here is a list of coins you can add to your wallet
+          </LandingPageText>
+        </LandingPageHeader>
+
         <CryptoCoinIndex />
       </CryptoCoinIndexWrapper>
     </StyledLandingPage>
--- client/src/pages/LandingPage/styles.js
+++ client/src/pages/LandingPage/styles.js
@@ -10,3 +10,13 @@ export const StyledLandingPage = styled.div`
 export const CryptoCoinIndexWrapper = styled.div`
   width: 65%;
 `;
+
+export const LandingPageHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 40px;
+`;
+
+export const LandingPageText = styled.h1``;
