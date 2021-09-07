6439a42e3f4632cbac8440f3637205f2f97bf019
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2021-09-06 23:39:08 -0400

    tesdt

--- server/server.js
+++ server/server.js
@@ -39,7 +39,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("/app/client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/app/client/build/index.html"));
+    res.sendFile(path.resolve(__dirname + "/app/client/build/index.html"));
   });
 }
+
 module.exports = app;
