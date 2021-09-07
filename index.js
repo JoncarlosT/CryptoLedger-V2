576afa9211fefa818b607fe27d17bd54471812cd
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2021-09-06 22:39:58 -0400

    test

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const app = require("./server/server");
 const PORT = process.env.PORT || 5000;
+const path = require("path");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT:${PORT}`);
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
--- server/server.js
+++ server/server.js
@@ -6,10 +6,6 @@ const { graphqlHTTP } = require("express-graphql");
 const cors = require("cors");
 const model = require("./model");
 const schema = require("./schema/schema");
-const path = require("path");
-const dotenv = require("dotenv");
-
-dotenv.config();
 
 if (!DB_CONNECTION) {
   throw new Error("You must provide correct key to connect to DB");
@@ -34,15 +30,5 @@ app.use(
     schema,
   })
 );
-console.log("hit1");
-
-if (process.env.NODE_ENV === "production") {
-  console.log("hit");
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
 
 module.exports = app;
