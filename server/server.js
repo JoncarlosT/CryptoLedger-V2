0972eecf8a738e09d8298f70b71c6f472542abfe
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2021-09-06 20:34:25 -0400

    hosting test

--- server/server.js
+++ server/server.js
@@ -32,4 +32,12 @@ app.use(
   })
 );
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 module.exports = app;
