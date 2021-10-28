25ec0e7a529b67eafd0340c68e1c4b29aa66dd22
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2021-10-27 21:22:56 -0400

    Authpage fixes

--- client/src/pages/AuthPage/styles.js
+++ client/src/pages/AuthPage/styles.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 export const StyledAuthPage = styled.div`
-  /* height: 100vh; */
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   padding-top: 50px;
+  height: 70vh;
 `;
 
 export const AuthFormWrapper = styled.div`
