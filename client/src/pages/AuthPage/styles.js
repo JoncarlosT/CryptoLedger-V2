6f196d451cd185a3a30495fff289af70afb8c151
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2021-11-01 20:09:30 -0400

    Authpage slit into left rigth side

--- client/src/pages/AuthPage/styles.js
+++ client/src/pages/AuthPage/styles.js
@@ -2,17 +2,40 @@ import styled from "styled-components";
 
 export const StyledAuthPage = styled.div`
   display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const LeftSide = styled.div`
+  height: 90.2vh;
+  width: 50vw;
+  display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding-top: 50px;
-  height: 70vh;
+`;
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const RightSide = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  height: 90.2vh;
+  width: 50vw;
 `;
 
 export const AuthFormWrapper = styled.div`
   padding: 20px;
   border-radius: 15px;
-  background-color: red;
+  margin-bottom: 100px;
+  border: solid 3px;
 `;
 
 export const AuthNavigationWrapper = styled.div`
