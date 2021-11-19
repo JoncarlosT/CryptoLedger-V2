4e60932dcfd0308c0a93e2a78ee4413a12d38b0b
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2021-11-18 21:17:47 -0500

    navbar development

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import coinData from "../../data/coin_storage.json";
 import { createFilter } from "react-select";
-
+import MenuIcon from "@material-ui/icons/Menu";
+import { Menu, MenuItem } from "@material-ui/core";
 import {
   StyledNavbar,
   NavLink,
@@ -10,6 +11,9 @@ import {
   LogoImage,
   ButtonWrapper,
   MobileLogoImage,
+  MobileIcon,
+  Span,
+  MobileMenuItem,
 } from "./styles";
 import { useQuery } from "@apollo/client";
 import { IS_LOGGED_IN } from "../../graphql/queries";
@@ -21,14 +25,17 @@ import Logo from "../../assets/CL_White_Logo.png";
 
 const Navbar = () => {
   const [mobile, setMobile] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
 
   const history = useHistory();
 
   const { data } = useQuery(IS_LOGGED_IN);
 
   const checkPageSize = () => {
-    if (window.innerWidth > parseInt(size.mobileL)) setMobile(true);
-    else setMobile(false);
+    if (window.innerWidth > parseInt(size.mobileL)) {
+      setMobile(true);
+      handleClose();
+    } else setMobile(false);
   };
 
   useEffect(() => {
@@ -36,6 +43,14 @@ const Navbar = () => {
   }, []);
 
   window.addEventListener("resize", checkPageSize);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <StyledNavbar>
@@ -48,7 +63,6 @@ const Navbar = () => {
           <MobileLogoImage src={Combine_Logo} alt="Logo" />
         </NavLink>
       )}
-
       <SearchBar
         filterOption={createFilter({ ignoreAccents: false })}
         options={coinData}
@@ -62,16 +76,62 @@ const Navbar = () => {
       />
 
       {data.isLoggedIn ? (
-        <ButtonWrapper>
-          <NavLink to="/user">
-            <StyledButton width={"100px"}>Wallet</StyledButton>
-          </NavLink>
-          <LogoutButton userId={JSON.parse(data.userData)._id} />
-        </ButtonWrapper>
-      ) : (
+        mobile ? (
+          <ButtonWrapper>
+            <NavLink to="/user">
+              <StyledButton width={"100px"}>Wallet</StyledButton>
+            </NavLink>
+            <LogoutButton userId={JSON.parse(data.userData)._id} />
+          </ButtonWrapper>
+        ) : (
+          <>
+            <MobileIcon onClick={handleClick}>
+              <Span>
+                <MenuIcon />
+              </Span>
+            </MobileIcon>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>
+                <MobileMenuItem to="/user">Wallet</MobileMenuItem>
+              </MenuItem>
+
+              <MenuItem onClick={handleClose}>
+                <MobileMenuItem to="/Logout">
+                  <LogoutButton userId={JSON.parse(data.userData)._id} />
+                </MobileMenuItem>
+              </MenuItem>
+            </Menu>
+          </>
+        )
+      ) : mobile ? (
         <NavLink to="/auth">
           <StyledButton width={"100px"}>Log In</StyledButton>
         </NavLink>
+      ) : (
+        <>
+          <MobileIcon onClick={handleClick}>
+            <Span>
+              <MenuIcon />
+            </Span>
+          </MobileIcon>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>
+              <MobileMenuItem to="/auth">Log In</MobileMenuItem>
+            </MenuItem>
+          </Menu>
+        </>
       )}
     </StyledNavbar>
   );
