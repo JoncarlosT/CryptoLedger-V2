e08d486d3e9d3606399f8d6af4b04ad15103b290
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2021-12-28 21:03:34 -0500

    adding coin error handling

--- client/src/components/AddCoinMenu/AddCoinMenu.js
+++ client/src/components/AddCoinMenu/AddCoinMenu.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import { AmountInput, StyledAddCoinMenu } from "./styles";
 import { useMutation } from "@apollo/client";
 import { ADD_COIN_TO_USER_WALLET } from "../../graphql/mutations";
 import StyledButton from "../StyledButton/StyledButton";
 
 const AddCoinMenu = ({ coinData, userData }) => {
-  const [amount, setAmount] = useState(undefined);
+  const [amount, setAmount] = useState(0);
+  const [showError, setShowError] = useState(false);
 
   const [addCoinToUserWallet, { called }] = useMutation(
     ADD_COIN_TO_USER_WALLET,
@@ -20,20 +22,30 @@ const AddCoinMenu = ({ coinData, userData }) => {
   );
 
   return (
-    <div>
-      <input
+    <StyledAddCoinMenu>
+      {showError ? <h1>Enter Number</h1> : <></>}
+
+      <AmountInput
         value={amount}
-        onChange={(e) => setAmount(parseInt(e.target.value))}
+        onChange={(e) => setAmount(parseInt(e.target.value) || amount)}
         type="number"
         placeholder="Amount"
       />
 
-      <StyledButton onClick={() => addCoinToUserWallet()}>
+      <StyledButton
+        onClick={() => {
+          if (amount === 0) {
+            setShowError(!showError);
+          } else {
+            addCoinToUserWallet();
+          }
+        }}
+      >
         Add Coin
       </StyledButton>
 
       {called ? <h1>Added</h1> : <></>}
-    </div>
+    </StyledAddCoinMenu>
   );
 };
 
---
+++ client/src/components/AddCoinMenu/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StyledAddCoinMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+export const AmountInput = styled.input`
+  margin-bottom: 30px;
+  height: 45px;
+  border-style: none;
+  font-size: 1em;
+  text-align: center;
+`;
