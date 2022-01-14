dd8f36fc4f8519fcd60d1a3df2cdbd08e0b619b3
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2022-01-13 21:09:06 -0500

    Userpage navigation to crypto index

--- client/src/pages/UserPage/styles.js
+++ client/src/pages/UserPage/styles.js
@@ -1,10 +1,15 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 import { device } from "../../device";
 
 export const StyledUserPage = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+
+export const NavLink = styled(Link)`
+  text-decoration: none;
 `;
 
 export const UserCryptoChart = styled.div`
